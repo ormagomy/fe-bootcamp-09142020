1e0e2f2bcfb6c6bdce7070793746159508587540
jorme <jared_orme@intuit.com>
2020-09-17 15:38:13 -0600

    Move edit & cancel functionality back to CarTool out of useCarList hook

--- src/components/CarTool.tsx
+++ src/components/CarTool.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core';
 
-import { Car } from '../models/Cars';
+import { Car, CarFormData } from '../models/Cars';
 import { CarForm } from './CarForm';
 import { CarTable } from './CarTable';
 import { ToolHeader } from './ToolHeader';
@@ -23,13 +23,31 @@ const useStyles = makeStyles({
 
 export function CarTool({ cars: defaultCars, colors }: CarToolProps) {
     const classes = useStyles();
-    const [cars, carToEdit, addCar, setCarToEdit, saveCarEdit, cancelEdit, deleteCar] = useCarList(defaultCars);
+    const [cars, addCar, replaceCar, deleteCar] = useCarList(defaultCars);
+    const [carToEdit, setCarToEdit] = useState<Car>();
+
+    const onAddCar = (carForm: CarFormData) => {
+        addCar(carForm);
+        cancelEdit();
+    };
+
+    const onDeleteCar = (carId: number) => {
+        deleteCar(carId);
+        cancelEdit();
+    };
+
+    const saveCarEdit = (carToSave: Car) => {
+        replaceCar(carToSave);
+        cancelEdit();
+    };
+
+    const cancelEdit = () => setCarToEdit(undefined);
 
     return (
         <div className={classes.root}>
             <ToolHeader headerText="Car tool" />
-            <CarTable cars={cars} carToEdit={carToEdit} colors={colors} onEditCar={setCarToEdit} onSaveEdit={saveCarEdit} onCancelEdit={cancelEdit} onDeleteCar={deleteCar} />
-            <CarForm buttonText="Add Car" colors={colors} onAddCar={addCar} />
+            <CarTable cars={cars} carToEdit={carToEdit} colors={colors} onEditCar={setCarToEdit} onSaveEdit={saveCarEdit} onCancelEdit={cancelEdit} onDeleteCar={onDeleteCar} />
+            <CarForm buttonText="Add Car" colors={colors} onAddCar={onAddCar} />
         </div>
     );
 }
