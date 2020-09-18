e33990b8971d2a1b7967795030834d001bea2351
jorme <jared_orme@intuit.com>
2020-09-18 15:32:36 -0600

    CalcTool basics

--- calc-app/src/index.tsx
+++ calc-app/src/index.tsx
@@ -1,17 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Action, Reducer, createStore, bindActionCreators } from 'redux';
+import { useSelector, useDispatch, Provider } from 'react-redux';
+
+const ADD_ACTION = 'ADD';
+const SUBTRACT_ACTION = 'SUBTRACT';
+
+export interface CalcOpAction extends Action {
+    payload: { num: number };
+}
+
+export type CalcOpActionCreator = (num: number) => CalcOpAction;
+
+export const createAddAction: CalcOpActionCreator = num => ({
+    type: ADD_ACTION,
+    payload: { num },
+});
+
+export const createSubtractAction: CalcOpActionCreator = num => ({
+    type: SUBTRACT_ACTION,
+    payload: { num },
+});
+
+export type CalcToolState = {
+    result: number;
+};
+
+export const calcToolReducer: Reducer<CalcToolState, CalcOpAction> = (state = { result: 0 }, action) => {
+    switch (action.type) {
+        case ADD_ACTION:
+            return {
+                ...state,
+                result: state.result + action.payload.num,
+            };
+        case SUBTRACT_ACTION:
+            return {
+                ...state,
+                result: state.result - action.payload.num,
+            };
+        default:
+            return state;
+    }
+};
+
+export const calcToolStore = createStore(calcToolReducer);
+
+type CalcToolProps = {
+    result: number;
+    onAdd: (num: number) => void;
+    onSubtract: (num: number) => void;
+};
+
+export function CalcTool({ result, onAdd, onSubtract }: CalcToolProps) {
+    const [numInput, setNumInput] = useState(0);
+
+    return (
+        <>
+            <div>
+                Result: <span>{result}</span>
+            </div>
+            <div>
+                <label>
+                    Num Input: <input type="number" value={numInput} onChange={e => setNumInput(e.target.valueAsNumber)} />
+                </label>
+            </div>
+            <fieldset>
+                <button type="button" onClick={() => onAdd(numInput)}>
+                    +
+                </button>
+                <button type="button" onClick={() => onSubtract(numInput)}>
+                    -
+                </button>
+            </fieldset>
+        </>
+    );
+}
+
+export function CalcToolContainer() {
+    const result = useSelector<CalcToolState, number>(state => state.result);
+    const boundActionsMap = bindActionCreators(
+        {
+            onAdd: createAddAction,
+            onSubtract: createSubtractAction,
+        },
+        useDispatch()
+    );
+
+    return <CalcTool result={result} {...boundActionsMap} />;
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={calcToolStore}>
+        <CalcToolContainer />
+    </Provider>,
+    document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
