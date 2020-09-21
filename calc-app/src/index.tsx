acfa7ede322fe0e3df299642ee42bb2642f073f0
jorme <jared_orme@intuit.com>
2020-09-21 11:25:12 -0600

    Exercise 16: delete history, validation error

--- calc-app/src/index.tsx
+++ calc-app/src/index.tsx
@@ -8,6 +8,7 @@ const SUBTRACT_ACTION = 'SUBTRACT';
 const MULTIPLY_ACTION = 'MULTIPLY';
 const DIVIDE_ACTION = 'DIVIDE';
 const CLEAR_ACTION = 'CLEAR';
+const DELETE_HISTORY_ACTION = 'DELETE_HISTORY';
 
 interface CalcOpAction extends Action {
     payload: { num: number };
@@ -35,20 +36,32 @@ const createDivideAction: CalcOpActionCreator = num => ({
     payload: { num },
 });
 
-type ClearActionCreator = () => Action;
+type SimpleActionCreator = () => Action;
 
-const createClearAction: ClearActionCreator = () => ({
+const createClearAction: SimpleActionCreator = () => ({
     type: CLEAR_ACTION,
 });
 
+interface DeleteHistoryAction extends Action {
+    payload: { index: number };
+}
+
+type DeleteHistoryActionCreator = (index: number) => DeleteHistoryAction;
+
+const createDeleteAction: DeleteHistoryActionCreator = index => ({
+    type: DELETE_HISTORY_ACTION,
+    payload: { index },
+});
+
 type CalcToolHistory = {
-    operation: '+' | '-' | '*' | '/';
+    operation: string;
     value: number;
 };
 
 type CalcToolState = {
     result: number;
     history: CalcToolHistory[];
+    validationError?: string;
 };
 
 const defaultState = {
@@ -59,58 +72,94 @@ const defaultState = {
 function isCalcOpAction(action: Action<string>): action is CalcOpAction {
     return action.type === ADD_ACTION || action.type === SUBTRACT_ACTION || action.type === MULTIPLY_ACTION || action.type === DIVIDE_ACTION;
 }
+function isDeleteHistoryAction(action: Action<string>): action is DeleteHistoryAction {
+    return action.type === DELETE_HISTORY_ACTION;
+}
 
 const calcToolReducer: Reducer<CalcToolState, CalcOpAction | Action> = (state = defaultState, action) => {
     if (isCalcOpAction(action)) {
+        if (action.payload.num < 0 || action.payload.num > 10) {
+            return {
+                ...state,
+                validationError: 'Input number must be between 0 and 10',
+            };
+        }
+
+        const newState = {
+            ...state,
+            validationError: undefined,
+            history: [...state.history, { operation: action.type, value: action.payload.num }],
+        };
+
         switch (action.type) {
             case ADD_ACTION:
-                return {
-                    ...state,
-                    result: state.result + action.payload.num,
-                    history: [...state.history, { operation: '+', value: action.payload.num }],
-                };
+                newState.result = state.result + action.payload.num;
+                return newState;
             case SUBTRACT_ACTION:
-                return {
-                    ...state,
-                    result: state.result - action.payload.num,
-                    history: [...state.history, { operation: '-', value: action.payload.num }],
-                };
+                newState.result = state.result - action.payload.num;
+                return newState;
             case MULTIPLY_ACTION:
-                return {
-                    ...state,
-                    result: state.result * action.payload.num,
-                    history: [...state.history, { operation: '*', value: action.payload.num }],
-                };
+                newState.result = state.result * action.payload.num;
+                return newState;
             case DIVIDE_ACTION:
-                return {
-                    ...state,
-                    result: state.result / action.payload.num,
-                    history: [...state.history, { operation: '/', value: action.payload.num }],
-                };
+                newState.result = state.result / action.payload.num;
+                return newState;
         }
-    } else if (action.type === CLEAR_ACTION) {
+    }
+
+    if (isDeleteHistoryAction(action)) {
+        const updatedHistory = [...state.history];
+        updatedHistory.splice(action.payload.index, 1);
         return {
-            ...defaultState,
+            ...state,
+            history: updatedHistory,
         };
     }
-    // fallthrough case
-    return state;
+
+    switch (action.type) {
+        case CLEAR_ACTION:
+            return {
+                ...defaultState,
+            };
+        default:
+            return state;
+    }
 };
 
 const calcToolStore = createStore(calcToolReducer);
 
+const mapOpActionToString = (actionType: string) => {
+    switch (actionType) {
+        case ADD_ACTION:
+            return '+';
+        case SUBTRACT_ACTION:
+            return '-';
+        case MULTIPLY_ACTION:
+            return '*';
+        case DIVIDE_ACTION:
+            return '/';
+    }
+};
+
 type CalcToolProps = {
     result: number;
     history: CalcToolHistory[];
+    validationError?: string;
     onAdd: (num: number) => void;
     onSubtract: (num: number) => void;
     onMultiply: (num: number) => void;
     onDivide: (num: number) => void;
     onClear: () => void;
+    onDeleteHistory: (num: number) => void;
 };
 
-function CalcTool({ result, history, onAdd, onSubtract, onMultiply, onDivide, onClear }: CalcToolProps) {
+function CalcTool({ result, history, validationError, onAdd, onSubtract, onMultiply, onDivide, onClear, onDeleteHistory }: CalcToolProps) {
     const [numInput, setNumInput] = useState(0);
+
+    const clear = () => {
+        setNumInput(0);
+        onClear();
+    };
 
     return (
         <>
@@ -122,6 +171,7 @@ function CalcTool({ result, history, onAdd, onSubtract, onMultiply, onDivide, on
                     Num Input: <input type="number" value={numInput} onChange={e => setNumInput(e.target.valueAsNumber)} />
                 </label>
             </div>
+            {validationError && <div>{validationError}</div>}
             <fieldset>
                 <button type="button" onClick={() => onAdd(numInput)}>
                     +
@@ -135,22 +185,27 @@ function CalcTool({ result, history, onAdd, onSubtract, onMultiply, onDivide, on
                 <button type="button" onClick={() => onDivide(numInput)}>
                     /
                 </button>
+                <button onClick={clear}>Clear</button>
             </fieldset>
             <ul>
-                {history.map(item => (
-                    <li>
-                        {item.operation} {item.value}
+                {history.map((item, index) => (
+                    <li key={index}>
+                        {mapOpActionToString(item.operation)} {item.value}
+                        &nbsp;&nbsp;&nbsp;
+                        <button onClick={() => onDeleteHistory(index)}>Delete</button>
                     </li>
                 ))}
             </ul>
-            <button onClick={onClear}>Clear</button>
         </>
     );
 }
 
 function CalcToolContainer() {
-    const result = useSelector<CalcToolState, number>(state => state.result);
-    const history = useSelector<CalcToolState, CalcToolHistory[]>(state => state.history);
+    const stateMap = {
+        result: useSelector<CalcToolState, number>(state => state.result),
+        history: useSelector<CalcToolState, CalcToolHistory[]>(state => state.history),
+        validationError: useSelector<CalcToolState, string | undefined>(state => state.validationError),
+    };
 
     /* This is creating dispatch bindings like this:
      *   onAdd: (num) => dispatch(createAddAction(num))
@@ -163,11 +218,12 @@ function CalcToolContainer() {
             onMultiply: createMultiplyAction,
             onDivide: createDivideAction,
             onClear: createClearAction,
+            onDeleteHistory: createDeleteAction,
         },
         useDispatch()
     );
 
-    return <CalcTool result={result} history={history} {...boundActionsMap} />;
+    return <CalcTool {...stateMap} {...boundActionsMap} />;
 }
 
 ReactDOM.render(
