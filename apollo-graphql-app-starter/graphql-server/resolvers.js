c9fff1e85ec17faa42584e689c38d9f0956db1fa
jorme <jared_orme@intuit.com>
2020-09-23 11:04:22 -0600

    Added apollo-graphql-app-starter

---
+++ apollo-graphql-app-starter/graphql-server/resolvers.js
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch';
+
+export const resolvers = {
+  Query: {
+    message: () => 'Hello World!',
+    headerText: () => 'The Tools',
+    colors: () => {
+      return fetch('http://localhost:3040/colors')
+        .then(res => res.json());
+    }
+  },
+};
