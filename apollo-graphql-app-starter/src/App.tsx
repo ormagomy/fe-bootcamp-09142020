3f89979605ac07e130d942dff385efc5c6f3f85f
jorme <jared_orme@intuit.com>
2020-09-23 12:48:31 -0600

    Exercise 2: gql cars table

--- apollo-graphql-app-starter/src/App.tsx
+++ apollo-graphql-app-starter/src/App.tsx
@@ -1,15 +1,32 @@
 import * as React from 'react';
 import { useQuery } from 'react-apollo';
 import gql from 'graphql-tag';
+import { Car } from './models/car';
 
 const APP_QUERY = gql`
   query AppQuery {
-    message
+    headerText
+    colors {
+      id
+      name
+    }
+    cars {
+      id
+      make
+      model
+      year
+      color
+      price
+    }
   }
 `;
 
+type Color = { id: number; name: string };
+
 export type AppQueryData = {
-  message: string,
+  headerText: string;
+  colors: Color[];
+  cars: Car[];
 };
 
 export const App = () => {
@@ -23,7 +40,39 @@ export const App = () => {
     return <div>Error: {error}</div>;
   }
 
-return <>
-    <h1>{data!.message}</h1>
-  </>;
+  return (
+    <>
+      <h1>{data!.headerText}</h1>
+      <ul>
+        {data?.colors.map(c => (
+          <li key={c.id}>{c.name}</li>
+        ))}
+      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Make</th>
+            <th>Model</th>
+            <th>Year</th>
+            <th>Color</th>
+            <th>Price</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.cars.map(car => (
+            <tr>
+              <td>{car.id}</td>
+              <td>{car.make}</td>
+              <td>{car.model}</td>
+              <td>{car.year}</td>
+              <td>{car.color}</td>
+              <td>{car.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 };
